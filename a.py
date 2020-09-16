811a406de739d08c3870568db835281f471ca8c3
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2020-09-16 19:31:35 +0530

    asked in Microsoft , google and uber

---
+++ a.py
@@ -0,0 +1,17 @@
+# Question asked by Microsft , uber and Microsoft 
+# it can be done in two ways 1. without sorting 2. with sorting
+
+
+def findtriplet(nums,n):
+    res = set()
+    nums.sort()
+    for i in range(0,n-2):
+        for j in range(i+1,n-1):
+            for k in range(j+1,n):
+                if (nums[i] + nums[j] + nums[k] == 0):
+                    res.add((nums[i],nums[j],nums[k]))
+    print (list(res))               
+    
+arr = [1, -2, 1, 0, 5]
+n = len(arr)
+findtriplet(arr,n)
